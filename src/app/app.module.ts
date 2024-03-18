015ccfee9c435cc71277eb154f4f732bd9cb2d9d
Alexis06cs <alexis06cs@gmail.com>
2024-03-17 21:14:30 -0500

    my firts commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AppRountingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    AppRountingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
